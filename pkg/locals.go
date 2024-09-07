fec23a7c05b67fd570d65bcfeabf9132318d6b70
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-07 19:36:45 +0530

    implemented module

---
+++ pkg/locals.go
@@ -0,0 +1,28 @@
+package pkg
+
+import (
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awssecretsmanager"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/commons/apiresource/enums/apiresourcekind"
+	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/aws/awstagkeys"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"strconv"
+)
+
+type Locals struct {
+	AwsSecretsManager *awssecretsmanager.AwsSecretsManager
+	AwsTags           map[string]string
+}
+
+func initializeLocals(ctx *pulumi.Context, stackInput *awssecretsmanager.AwsSecretsManagerStackInput) *Locals {
+	locals := &Locals{}
+	locals.AwsSecretsManager = stackInput.ApiResource
+
+	locals.AwsTags = map[string]string{
+		awstagkeys.Resource:     strconv.FormatBool(true),
+		awstagkeys.Organization: locals.AwsSecretsManager.Spec.EnvironmentInfo.OrgId,
+		awstagkeys.Environment:  locals.AwsSecretsManager.Spec.EnvironmentInfo.EnvId,
+		awstagkeys.ResourceKind: apiresourcekind.ApiResourceKind_aws_secrets_manager.String(),
+		awstagkeys.ResourceId:   locals.AwsSecretsManager.Metadata.Id,
+	}
+	return locals
+}
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,20 @@
 package outputs
 
 import (
+	"fmt"
 	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awssecretsmanager"
+	"github.com/plantoncloud/stack-job-runner-golang-sdk/pkg/automationapi/autoapistackoutput"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
 func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
 	input *awssecretsmanager.AwsSecretsManagerStackInput) *awssecretsmanager.AwsSecretsManagerStackOutputs {
-	return &awssecretsmanager.AwsSecretsManagerStackOutputs{}
+	stackOutputs := &awssecretsmanager.AwsSecretsManagerStackOutputs{
+		SecretArnMap: make(map[string]string),
+	}
+	for _, secretName := range input.ApiResource.Spec.SecretNames {
+		stackOutputs.SecretArnMap[secretName] = autoapistackoutput.GetVal(pulumiOutputs,
+			fmt.Sprintf("%s-arn", secretName))
+	}
+	return stackOutputs
 }
